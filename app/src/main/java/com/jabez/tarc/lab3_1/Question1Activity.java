93ac01cb07d74c29f97a071020706457918d5121
Jabezyong <jabez.yong1215@gmail.com>
2016-11-01 10:07:04 +0800

    Initial commit

---
+++ app/src/main/java/com/jabez/tarc/lab3_1/Question1Activity.java
@@ -0,0 +1,13 @@
+package com.jabez.tarc.lab3_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Question1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question1);
+    }
+}
---
+++ app/src/main/java/com/jabez/tarc/lab3_1/SearchActivity.java
@@ -0,0 +1,14 @@
+package com.jabez.tarc.lab3_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
